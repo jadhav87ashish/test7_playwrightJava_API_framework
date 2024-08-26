4808deb9523d2914e5d5bbb64697722537d3a97b
Ashish Jadhav <jadhav87ashish@gmail.com>
2024-08-26 23:57:41 +0530

    GET request config playwright java

--- src/main/java/utility/APISetup.java
+++ src/main/java/utility/APISetup.java
@@ -1,6 +1,9 @@
 package utility;
 
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.options.RequestOptions;
 
 import java.util.Map;
 
@@ -19,9 +22,31 @@ public class APISetup {
         String thirdKey;
         String fourthKey;
         String formattedURL;
+        if (endurl.contains("%s")){
+            formattedURL=String.format(endurl,input);
+        } else {
+            formattedURL = endurl;
+        }
+        switch (variables.length){
+            case 1:
+                System.out.println("Single Param");
+                String[] parts = input.split("\\W+");
+                return setAPIRequestContext().get(formattedURL, RequestOptions.create().setQueryParam(parts[0],parts[1]));
+
+
+
+        }
+
 
 
         return null;
+    }
+
+    private APIRequestContext setAPIRequestContext() {
+        System.out.println(setBaseURL);
+        return FrameworkConfig.playwright.request().newContext(new APIRequest().NewContextOptions())
+                .setBaseURL(setBaseURL()).setExtraHTTPHeaders(setHeaders()).setTimeout(30000));
+
     }
 
 
